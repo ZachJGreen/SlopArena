aea3f25a0285cbf2876005cddc83e441a2b05704
ZachJGreen <green.zach.j@gmail.com>
2026-09-09 23:46:25 -0500

    added player movement and jump feature

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,14 +4,16 @@ using UnityEngine.InputSystem;
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 12f;
-    [SerializeField] private float gravity = 9.8f;
+    [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private float jumpPower = 5f;
     [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference jumpAction;
     private CharacterController characterControl;
     private Vector2 playerInput;
     private Vector3 moveDirection;
     private float xMovement;
     private float zMovement;
-
+    private float verticalVelocity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,10 +24,24 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (characterControl.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -2f;
+        }
+
+        if (characterControl.isGrounded && jumpAction.action.WasPressedThisFrame())
+        {
+            verticalVelocity = jumpPower;
+        }
+        verticalVelocity += gravity * Time.deltaTime;
+
         playerInput = moveAction.action.ReadValue<Vector2>();
-        xMovement = playerInput.x;
-        zMovement = playerInput.y;
+        xMovement = playerInput.x * speed;
+        zMovement = playerInput.y * speed;
         moveDirection = (xMovement * transform.right) + (zMovement * transform.forward);
-        characterControl.Move(moveDirection * speed * Time.deltaTime);
+        moveDirection.y = (verticalVelocity);
+
+        characterControl.Move(moveDirection * Time.deltaTime);
+        
     }
 }
